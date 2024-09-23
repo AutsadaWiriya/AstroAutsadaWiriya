436593707aad14c99d8a43f5d7a4fe2d1296e683
AutsadaWiriya <autsadawiriya@gmail.com>
2024-09-23 22:34:36 +0700

    Edit

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'AutsadaWiriya',
-  subtitle: 'Demo Site',
+  subtitle: 'AutsadaWiriya Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 315,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
