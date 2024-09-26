35c0a932a2592360e4c4793ffedfad66787b2a4c
AutsadaWiriya <autsadawiriya@gmail.com>
2024-09-26 23:21:02 +0700

    Edit

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Profile',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 305,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 150,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
@@ -47,7 +47,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/ProfileX.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/profileDD.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Autsada Wiriya',
   bio: "Hi, I'm Aut.",
   links: [
